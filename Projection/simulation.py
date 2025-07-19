2cfff40bcb9d9a0207e98b7278cb7c820a086634
Freddie Bullard <freddie.bullard@outlook.com>
2025-07-19 18:59:08 +0100

    Housekeeping changes to simulation code

--- Projection/simulation.py
+++ Projection/simulation.py
@@ -1,11 +1,19 @@
+'''
+Simple simulated data with melanin components and non-melanin components.
+'''
+
+
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Assume melanin spectrum is already defined
+# Define melanin spectrum
 wavelengths = np.array([ 700.,  730.,  760.,  800.,  850.,  910.,  930.,  950.,  980.,
        1030., 1080., 1100.])
 melanin = 519 * (wavelengths / 500) ** (-3.5) * 0.4
+plt.plot(wavelengths, melanin / np.linalg.norm(melanin)**2)
+plt.show()
 
+# Initialise data matrix
 z, x, y = len(wavelengths), 400, 400
 data = np.zeros((z, x, y))
 
@@ -28,7 +36,7 @@ circle_right_center = (320, 270)
 mask_right = (xx - circle_right_center[0])**2 + (yy - circle_right_center[1])**2 <= circle_radius**2
 data[:, mask_right] = 50  # constant value across z
 
-# Optional: Visualise a few slices
+# Visualise a few slices
 fig, ax = plt.subplots(1, 3, figsize=(15, 5))
 for i, idx in enumerate([0, 6, 11]):
     ax[i].imshow(data[idx], cmap='viridis', clim=(0, 100))
